4d829e6b91b4e253159f8d4b81719c2c3fe8f741
KitsuneX07 <144137119+KitsuneX07@users.noreply.github.com>
2025-11-14 12:36:52 +0800

    Update GitHub and Bilibili URLs in config

--- src/config.ts
+++ src/config.ts
@@ -234,21 +234,15 @@ export const navBarConfig: NavBarConfig = {
 			children: [
 				{
 					name: "GitHub",
-					url: "https://github.com/matsuzaka-yuki/Mizuki",
+					url: "https://github.com/KitsuneX07",
 					external: true,
 					icon: "fa6-brands:github",
 				},
 				{
 					name: "Bilibili",
-					url: "https://space.bilibili.com/701864046",
+					url: "https://space.bilibili.com/403335715",
 					external: true,
 					icon: "fa6-brands:bilibili",
-				},
-				{
-					name: "Gitee",
-					url: "https://gitee.com/matsuzakayuki/Mizuki",
-					external: true,
-					icon: "mdi:git",
 				},
 			],
 		},
